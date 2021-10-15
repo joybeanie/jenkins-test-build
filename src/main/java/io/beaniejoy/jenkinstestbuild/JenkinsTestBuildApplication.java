8192503261e897e6ee31239cd78fe1fa5b91d24c
beaniejoy <lhb1025@gmail.com>
2021-10-16 00:13:50 +0900

    first commit

---
+++ src/main/java/io/beaniejoy/jenkinstestbuild/JenkinsTestBuildApplication.java
@@ -0,0 +1,13 @@
+package io.beaniejoy.jenkinstestbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestBuildApplication.class, args);
+	}
+
+}
